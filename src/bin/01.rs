87b1fec5d7de2d7257792e0409353cf05d5c5195
Stefan Bauckmeier <stefan.bauckmeier@evernest.com>
2023-12-01 18:40:23 +0100

    feat: 01/02

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,5 +1,7 @@
 advent_of_code::solution!(1);
 
+use regex::Regex;
+
 pub fn part_one(input: &str) -> Option<u32> {
     let mut calibration_sum: i32 = 0;
     let lines: Vec<&str> = input.lines().collect();
@@ -19,8 +21,45 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(calibration_sum as u32)
 }
 
+fn word_to_number(word: &str) -> &str {
+    match word {
+        "one" => "1",
+        "two" => "2",
+        "three" => "3",
+        "four" => "4",
+        "five" => "5",
+        "six" => "6",
+        "seven" => "7",
+        "eight" => "8",
+        "nine" => "9",
+        _ => word,
+    }
+}
+
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let mut calibration_sum: u32 = 0;
+    let number_matcher = Regex::new(r"^([0-9]|one|two|three|four|five|six|seven|eight|nine)").unwrap();
+    let lines: Vec<&str> = input.lines().collect();
+
+    for line in lines {
+        let mut str_numbers:Vec<&str> = [].to_vec();
+
+        // we cannot just match regex since numbers like "seveneight" would just match "seven", but we need "seven" and "eight"
+        for start_pos in 0..line.len() {
+            if number_matcher.is_match(line.split_at(start_pos).1) {
+                str_numbers.push(number_matcher.find(line.split_at(start_pos).1).unwrap().as_str());
+            }
+        }
+
+        let calibration_value = [
+            word_to_number(str_numbers[0]),
+            word_to_number(str_numbers.last().unwrap())
+        ].join("").parse::<u32>().unwrap();
+
+        calibration_sum += calibration_value;
+    }
+
+    Some(calibration_sum)
 }
 
 #[cfg(test)]
