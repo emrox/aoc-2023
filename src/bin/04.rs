3e15180d998af18ad36ad0df670ef857c403c324
Stefan Bauckmeier <github@elektronische-musik.de>
2023-12-04 14:45:57 +0100

    feat: 04/02

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -37,8 +37,34 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(total_points)
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u32> {
+    let lines: Vec<&str> = input.lines().collect();
+    let lines_count = lines.len() as u32;
+    let mut game_copies: Vec<u32> = (0..lines_count as u32).map(|_| 1 as u32).collect();
+
+    for line_number in 0..lines_count {
+        let line = lines[line_number as usize];
+
+        let game_split = line.split(':').collect::<Vec<&str>>();
+        let number_split = game_split[1].split('|').collect::<Vec<&str>>();
+
+        let winning_numbers = get_numbers_from_string(number_split[0]);
+        let game_numbers = get_numbers_from_string(number_split[1]);
+
+        let item_set: HashSet<&u32> = winning_numbers.iter().collect();
+        let difference: Vec<u32> = game_numbers.into_iter().filter(|item| item_set.contains(item)).collect();
+
+        let matching_count = difference.len() as u32;
+        let max_add_copy_game = if matching_count > lines_count - line_number - 1 { lines_count - line_number - 1 as u32 } else { matching_count };
+
+        for _game_number in 0..game_copies[line_number as usize] {
+            for add_to_game in 0..max_add_copy_game {
+                game_copies[(line_number + add_to_game + 1) as usize] += 1;
+            }
+        }
+    }
+
+    Some(game_copies.iter().sum())
 }
 
 #[cfg(test)]
@@ -54,6 +80,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(30));
     }
 }
