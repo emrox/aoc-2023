daf8e1ab97f2e522e0483355a5be2e239ed7b8b1
Stefan Bauckmeier <github@elektronische-musik.de>
2023-12-06 11:46:57 +0100

    feat: 06/02

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -2,10 +2,10 @@ use regex::Regex;
 
 advent_of_code::solution!(6);
 
-fn get_numbers_from_string(string: &str) -> Vec<u32> {
+fn get_numbers_from_string(string: &str) -> Vec<u64> {
     let number_regex = Regex::new(r"([0-9]+)").unwrap();
 
-    number_regex.find_iter(string).map(|m| { m.as_str().parse::<u32>().unwrap() }).collect::<Vec<u32>>()
+    number_regex.find_iter(string).map(|m| { m.as_str().parse::<u64>().unwrap() }).collect::<Vec<u64>>()
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
@@ -15,7 +15,6 @@ pub fn part_one(input: &str) -> Option<u32> {
     let distances = get_numbers_from_string(lines[1]);
 
     let mut winning_ways: u32 = 1;
-
 
     for time_index in 0..times.len() {
         let max_time = times[time_index];
@@ -38,8 +37,24 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(winning_ways)
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u32> {
+    let lines: Vec<&str> = input.lines().collect();
+
+    let max_time = get_numbers_from_string(&lines[0].replace(" ", ""))[0] as u64;
+    let distance = get_numbers_from_string(&lines[1].replace(" ", ""))[0] as u64;
+
+    let mut possible_wins: u32 = 0;
+
+    for time in 1..max_time {
+        let travel_time = max_time - time;
+        let travel_speed = time;
+
+        if travel_time * travel_speed > distance {
+            possible_wins += 1;
+        }
+    }
+
+    Some(possible_wins)
 }
 
 #[cfg(test)]
@@ -55,6 +70,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(71503));
     }
 }
