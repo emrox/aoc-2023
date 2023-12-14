6d38a8f6cd785143cd28cb8cef3c667ad71ca208
Stefan Bauckmeier <github@elektronische-musik.de>
2023-12-14 20:57:24 +0100

    feat: 14/02 (but very slow)

--- src/bin/14.rs
+++ src/bin/14.rs
@@ -1,24 +1,105 @@
+use std::collections::BTreeMap;
+use itertools::Itertools;
+
 advent_of_code::solution!(14);
 
+type Field = Vec<Vec<char>>;
+
+fn tilt_north(plain_field: &Field, in_field: Field) -> Field {
+    let mut out_field: Field = plain_field.clone();
+
+    for row in 0..plain_field.len() {
+        for col in 0..in_field[row].len() {
+            if in_field[row][col] == 'O' {
+                let mut new_row = row;
+
+                while new_row > 0 && out_field[new_row - 1][col] == '.' {
+                    new_row -= 1;
+                }
+
+                out_field[new_row][col] = 'O';
+            }
+        }
+    }
+
+    out_field
+}
+
+fn tilt_west(plain_field: &Field, in_field: Field) -> Field {
+    let mut out_field: Field = plain_field.clone();
+
+    for row in 0..plain_field.len() {
+        for col in 0..in_field[row].len() {
+            if in_field[row][col] == 'O' {
+                let mut new_col = col;
+
+                while new_col > 0 && out_field[row][new_col - 1] == '.' {
+                    new_col -= 1;
+                }
+
+                out_field[row][new_col] = 'O';
+            }
+        }
+    }
+
+    out_field
+}
+
+fn tilt_south(plain_field: &Field, in_field: Field) -> Field {
+    let mut out_field: Field = plain_field.clone();
+    let max_row = plain_field.len() - 1;
+
+    for row in 0..plain_field.len() {
+        let rev_row = max_row - row;
+
+        for col in 0..in_field[row].len() {
+            if in_field[rev_row][col] == 'O' {
+                let mut new_row = rev_row;
+
+                while new_row < max_row && out_field[new_row + 1][col] == '.' {
+                    new_row += 1;
+                }
+
+                out_field[new_row][col] = 'O';
+            }
+        }
+    }
+
+    out_field
+}
+
+fn tilt_east(plain_field: &Field, in_field: Field) -> Field {
+    let mut out_field: Field = plain_field.clone();
+    let max_col = plain_field[0].len() - 1;
+
+    for row in 0..plain_field.len() {
+        for col in 0..in_field[row].len() {
+            let rev_col = max_col - col;
+
+            if in_field[row][rev_col] == 'O' {
+                let mut new_col = rev_col;
+
+                while new_col < max_col && out_field[row][new_col + 1] == '.' {
+                    new_col += 1;
+                }
+
+                out_field[row][new_col] = 'O';
+            }
+        }
+    }
+
+    out_field
+}
+
 pub fn part_one(input: &str) -> Option<u32> {
-    let field: Vec<Vec<char>> = input.lines().map(|l| { l.chars().collect() }).collect();
-    let mut tilted_field: Vec<Vec<char>> = field.iter().map(
+    let initial_field: Field = input.lines().map(|line| line.chars().collect_vec()).collect();
+    let plain_field: Field = initial_field.iter().map(
         |row| {
             row.iter().map(|c| return if c == &'O' { '.' } else { c.clone() }).collect()
         }
     ).collect();
 
-    for row in 0..field.len() {
-        for col in 0..field[row].len() {
-            if field[row][col] == 'O' {
-                let mut new_row = row;
-                while new_row > 0 && tilted_field[new_row - 1][col] == '.' {
-                    new_row -= 1;
-                }
-                tilted_field[new_row][col] = 'O';
-            }
-        }
-    }
+    let tilted_field = tilt_north(&plain_field, initial_field);
 
     let row_count = tilted_field.len();
     let mut load: u32 = 0;
@@ -33,8 +114,61 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(load)
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+fn str_to_field(field: &str) -> Field {
+    field.lines().map(|line| line.chars().collect_vec()).collect()
+}
+
+fn field_to_str(field: Field) -> String {
+    String::from(field.iter().map(|row| row.iter().collect::<String>()).join("\n"))
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let plain_field: Field = str_to_field(input).iter().map(
+        |row| {
+            row.iter().map(|c| return if c == &'O' { '.' } else { c.clone() }).collect()
+        }
+    ).collect();
+
+    let cycles = 1_000_000_000;
+
+    let mut tilted_field = input.to_string();
+    let mut field_cache: BTreeMap<String, String> = BTreeMap::new();
+
+    for _ in 0..cycles {
+        match field_cache.get(&tilted_field) {
+            Some(entry) => {
+                tilted_field = entry.to_string();
+                continue;
+            },
+            None => {}
+        }
+
+        let init_field = tilted_field.to_string();
+
+        let field = str_to_field(tilted_field.as_str());
+        let n = tilt_north(&plain_field, field);
+        let e = tilt_west(&plain_field, n);
+        let s = tilt_south(&plain_field, e);
+        let w = tilt_east(&plain_field, s);
+        let cycle_field = field_to_str(w);
+
+        tilted_field = cycle_field;
+
+        field_cache.insert(init_field, tilted_field.clone());
+    }
+
+    let tilted_field_field = str_to_field(tilted_field.as_str());
+    let row_count = tilted_field_field.len();
+    let mut load: u32 = 0;
+
+    for row in 0..row_count {
+        let occurences = tilted_field_field[row].iter().filter(|c| c == &&'O').count();
+        let line_load = occurences * (row_count - row);
+
+        load = load + (line_load as u32);
+    }
+
+    Some(load)
 }
 
 #[cfg(test)]
@@ -47,9 +181,10 @@ mod tests {
         assert_eq!(result, Some(136));
     }
 
-    #[test]
-    fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
-    }
+    // Takes too long to run, maybe I'll come back to it later
+    // #[test]
+    // fn test_part_two() {
+    //     let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+    //     assert_eq!(result, Some(64));
+    // }
 }
